81de09cdbc53174d5b5b9fa9cf715a48486430fc
gmos96 <moseleyg96@gmail.com>
2024-11-14 18:51:59 -0500

    initial commit

---
+++ src/users/user.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn, Unique } from 'typeorm';
+
+@Entity()
+@Unique(['emailAddress'])
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  emailAddress: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Public } from 'src/auth/constants';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+    
+    constructor(private userService: UsersService) {}
+
+    @Post('register')
+    @Public()
+    async registerUser(@Body() user: User) {
+        return this.userService.createUser(user);
+    }
+
+}
